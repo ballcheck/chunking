98767194771691cba02d9703eb02bf427d3c0e6c
Tim Brown <tim@maplejar.co.uk>
2013-07-14 11:08:22 +0100

    behavioral test split in to colourfast and non colourfast

--- test/unit/behavioral_test.rb
+++ test/unit/behavioral_test.rb
@@ -1,175 +1,215 @@
 # TODO: run these test again, inverting the colour.
 # TODO: run these tests again, swapping image library
 module BehavioralTest
-  def setup
-    @axis = :x
-    @rgb = [10000, 10000, 10000]
-    @background_rgb = nil
-    @foreground_rgb = @rgb
-  end
+  module Setup
+    def setup
+      @axis = :x
+      @rgb = [ 0, 0, 0 ]
+      @foreground_rgb = @rgb
+      @background_rgb = [ 65535, 65535, 65535 ]
+    end
 
-  def invert?
-    false
-  end
+    def invert?
+      false
+    end
 
-  def build_image_from_pixel_map( pixel_map )
-    Chunking::Image::RMagickImage.new_from_pixel_map pixel_map
-  end
-
-  def test_start_index
-    # given
-    o = @background_rgb
-    x = @foreground_rgb
-    pixel_map = [
-      [ o, o, o, o, o ],
-      [ x, x, x, x, x ],
-      [ o, o, o, o, o ],
-      [ x, x, x, x, x ],
-      [ o, o, o, o, o ],
-      [ o, o, o, o, o ],
-      [ x, x, x, x, x ],
-      [ o, o, o, o, o ]
-    ]
-
-    img = build_image_from_pixel_map pixel_map
-    img = img.rotate( -90 ) if @axis == :y
-    img = img.invert( @axis ) if invert?
-
-    detector = Chunking::Detector.new( :axis => @axis, :size => 5, :rgb => @rgb )
-
-    # on first row
-    assert_equal 1, detector.detect_boundary( img, 0, invert? ).index
-    # where initial row is boundary
-    assert_equal 3, detector.detect_boundary( img, 2, invert? ).index
-    # jump a row to boundary
-    assert_equal 6, detector.detect_boundary( img, 4, invert? ).index
-    # same boundary different start
-    assert_equal 6, detector.detect_boundary( img, 5, invert? ).index
-    # last row
-    assert_equal nil, detector.detect_boundary( img, 7, invert? )
-  end
-
-
-  def test_offset
-    o = @background_rgb
-    x = @foreground_rgb
-    pixel_map = [
-      [ o, o, o, o, o ],
-      [ x, o, o, o, o ],
-      [ o, x, o, o, o ],
-      [ o, o, x, o, o ],
-      [ o, o, o, x, o ],
-      [ o, o, o, o, x ]
-    ]
-
-    img = build_image_from_pixel_map pixel_map
-    img = img.rotate( -90 ) if @axis == :y
-  # TODO: needed this because the map doesn't work as offset is from top, not bottom.
-    #img = img.invert( :x ) if @axis == :y
-    img = img.invert( @axis ) if invert?
-
-    size = img.size( @axis )
-
-    size.times do |i|
-       detector = Chunking::Detector.new( :axis => @axis, :offset => i, :size => size - i, :rgb => @rgb )
-       assert_equal i+1,  detector.detect_boundary( img, 0, invert? ).index
+    def build_image_from_pixel_map( pixel_map )
+      Chunking::Image::RMagickImage.new_from_pixel_map pixel_map
     end
   end
 
-  # TODO: what should happen when detector bigger than image?
-  def test_size
-    o = @background_rgb
-    x = @foreground_rgb
-    pixel_map = [
-      [ o, o, o, o, o ],
-      [ o, o, o, o, x ],
-      [ o, o, o, x, o ],
-      [ o, o, x, o, o ],
-      [ o, x, o, o, o ],
-      [ x, o, o, o, o ]
-    ]
+  module ColourFast
+    include Setup
+    def test_start_index
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ x, x, x, x, x ],
+        [ o, o, o, o, o ],
+        [ o, o, o, o, o ],
+        [ x, x, x, x, x ],
+        [ o, o, o, o, o ]
+      ]
 
-    img = build_image_from_pixel_map pixel_map
-    img = img.rotate( -90 ) if @axis == :y
-  # TODO: needed this because the map doesn't work as offset is from top, not bottom.
-    #img = img.invert( :x ) if @axis == :y
-    img = img.invert( @axis ) if invert?
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
 
-    size = img.size( @axis )
+      detector = Chunking::Detector.new( :axis => @axis, :size => 5, :rgb => @rgb )
 
-    size.times do |i|
-       detector = Chunking::Detector.new( :axis => @axis, :size => size - i, :rgb => @rgb )
-       assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
+      # immediate boundary
+      assert_equal 1, detector.detect_boundary( img, 0, invert? ).index
+      # jump a row to boundary
+      assert_equal 4, detector.detect_boundary( img, 2, invert? ).index
+      # same boundary different start
+      assert_equal 4, detector.detect_boundary( img, 3, invert? ).index
+      # last row
+      assert_equal nil, detector.detect_boundary( img, 5, invert? )
+    end
+
+    def test_invert
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ x, x, x, x, x ],
+        [ o, o, o, o, o ],
+        [ o, o, o, o, o ],
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+
+      size = img.size( @axis )
+
+      detector = Chunking::Detector.new( :axis => @axis, :size => size, :rgb => @rgb )
+      assert_equal 1, detector.detect_boundary( img, 0, invert? ).index
+     
+      detector = Chunking::Detector.new( :axis => @axis, :size => size, :rgb => @rgb )
+      assert_equal 2, detector.detect_boundary( img, 0, !invert? ).index
+    end
+
+    def test_fuzz
+      # TODO: test fuzz
+    end
+
+    def test_no_matches
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ o, o, o, o, o ],
+        [ o, o, o, o, o ],
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+      size = img.size( @axis )
+
+      detector = Chunking::Detector.new( :axis => @axis, :size => size, :rgb => @rgb )
+      assert_equal nil, detector.detect_boundary( img, 0, invert? )
+    end
+
+    def test_all_matches
+      # this is covered by inverting bg/fg colours
+    end
+  end
+  
+  module NonColourFast
+    include Setup
+    def test_offset
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ x, o, o, o, o ],
+        [ x, x, o, o, o ],
+        [ x, x, x, o, o ],
+        [ x, x, x, x, o ],
+        [ x, x, x, x, x ]
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+
+      size = img.size( @axis )
+
+      size.times do |i|
+         detector = Chunking::Detector.new( :axis => @axis, :offset => i, :size => size - i, :rgb => @rgb )
+         assert_equal i+1,  detector.detect_boundary( img, 0, invert? ).index
+      end
+    end
+
+    # TODO: what should happen when detector bigger than image?
+    def test_size
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ o, o, o, o, x ],
+        [ o, o, o, x, x ],
+        [ o, o, x, x, x ],
+        [ o, x, x, x, x ],
+        [ x, x, x, x, x ]
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+
+      size = img.size( @axis )
+
+      size.times do |i|
+         detector = Chunking::Detector.new( :axis => @axis, :size => size - i, :rgb => @rgb )
+         assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
+      end
+    end
+
+    def test_density
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ o, o, x, o, o ],
+        [ o, x, x, o, o ],
+        [ o, x, x, x, o ],
+        [ x, x, x, x, o ],
+        [ x, x, x, x, x ]
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+
+      size = img.size( @axis )
+
+      5.times do |i|
+         detector = Chunking::Detector.new( :axis => @axis, :density => i + 1, :size => size, :rgb => @rgb )
+         assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
+      end
+    end
+
+    def test_tolerance
+      o = @background_rgb
+      x = @foreground_rgb
+      pixel_map = [
+        [ o, o, o, o, o ],
+        [ x, o, o, o, o ],
+        [ o, o, o, o, x ],
+        [ o, x, o, o, o ],
+        [ o, o, o, x, o ],
+        [ o, o, x, o, o ],
+      ]
+
+      img = build_image_from_pixel_map pixel_map
+      img = img.rotate( -90 ) if @axis == :y
+      img = img.invert( @axis ) if invert?
+
+      size = img.size( @axis )
+
+      5.times do |i|
+        detector = Chunking::Detector.new( :axis => @axis, :tolerance => i, :size => size, :rgb => @rgb )
+        assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
+      end
     end
   end
 
-  def test_density
-    o = @background_rgb
-    x = @foreground_rgb
-    pixel_map = [
-      [ o, o, o, o, o ],
-      [ o, o, x, o, o ],
-      [ o, x, x, o, o ],
-      [ o, x, x, x, o ],
-      [ x, x, x, x, o ],
-      [ x, x, x, x, x ]
-    ]
-
-    img = build_image_from_pixel_map pixel_map
-    img = img.rotate( -90 ) if @axis == :y
-    img = img.invert( @axis ) if invert?
-
-    size = img.size( @axis )
-
-    size.times do |i|
-       detector = Chunking::Detector.new( :axis => @axis, :density => i + 1, :size => size, :rgb => @rgb )
-       assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
-    end
-  end
-
-  def test_tolerance
-    o = @background_rgb
-    x = @foreground_rgb
-    pixel_map = [
-      [ o, o, o, o, o ],
-      [ x, o, o, o, o ],
-      [ o, o, o, o, x ],
-      [ o, x, o, o, o ],
-      [ o, o, o, x, o ],
-      [ o, o, x, o, o ],
-    ]
-
-    img = build_image_from_pixel_map pixel_map
-    img = img.rotate( -90 ) if @axis == :y
-    img = img.invert( @axis ) if invert?
-
-    size = img.size( @axis )
-
-    size.times do |i|
-       detector = Chunking::Detector.new( :axis => @axis, :tolerance => i, :size => size, :rgb => @rgb )
-       assert_equal i+1, detector.detect_boundary( img, 0, invert? ).index
-    end
-  end
-
-  def test_invert
-  end
-
-  def test_fuzz
-  end
-
-  def test_no_matches
-  end
-
-  def test_all_matches
+  module All
+    include BehavioralTest::ColourFast
+    include BehavioralTest::NonColourFast
   end
 end
 
 class BehavioralTestXAxis < ActiveSupport::TestCase
-  include BehavioralTest
+  include BehavioralTest::All
 end
 
 class BehavioralTestYAxis < ActiveSupport::TestCase
-  include BehavioralTest
+  include BehavioralTest::All
 
   def setup
     super
@@ -178,7 +218,7 @@ class BehavioralTestYAxis < ActiveSupport::TestCase
 end
 
 class BehavioralTestXAxisInvert < ActiveSupport::TestCase
-  include BehavioralTest
+  include BehavioralTest::All
 
   def invert?
     true
@@ -186,7 +226,7 @@ class BehavioralTestXAxisInvert < ActiveSupport::TestCase
 end
 
 class BehavioralTestYAxisInvert < ActiveSupport::TestCase
-  include BehavioralTest
+  include BehavioralTest::All
 
   def setup
     super
@@ -200,15 +240,12 @@ end
 
 # NOTE: starting on colour, moving off. 
 # TODO: this does not work on some tests
-=begin
 class BehavioralTestXAxisSwapFGBG < ActiveSupport::TestCase
-  include BehavioralTest
+  include BehavioralTest::ColourFast
 
   def setup
     super
-    #@rgb = [10000, 10000, 10000]
-    #@background_rgb = @rgb
-    #@foreground_rgb = nil
+    @background_rgb = @rgb
+    @foreground_rgb = nil
   end
 end
-=end
