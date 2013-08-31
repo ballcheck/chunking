9304e29ea5681e1423d1753cf7ae180bbbd375d2
Tim Brown <tim@maplejar.co.uk>
2013-08-31 06:25:58 +0100

    fixed runner so it does use rails. test now run in < 1 sec :)

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -1,4 +1,13 @@
-require File.expand_path( "../test_helper", __FILE__ )
+require 'active_support'
+require 'test/unit'
+
+class Rails
+  def self.root
+    "/home/tim/rails_projects/testing_project/"
+  end
+end
+
+#require File.expand_path( "../test_helper", __FILE__ )
 Dir.glob( File.expand_path( "../unit/chunking/**/*.rb", __FILE__ ), &method( :load ) )
 require Rails.root.to_s << "/lib/chunking/chunking"
 
@@ -6,3 +15,4 @@ class ActiveSupport::TestCase
   include Chunking::Factories
 end
 
+#Test::Unit::AutoRunner.run
