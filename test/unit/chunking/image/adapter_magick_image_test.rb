4c0878524ca023f4bac95170f99eb19c12bee747
Tim Brown <tim@maplejar.co.uk>
2014-01-30 07:18:50 +0000

    Removed some useless AdapterMagickImage tests

--- test/unit/chunking/image/adapter_magick_image_test.rb
+++ test/unit/chunking/image/adapter_magick_image_test.rb
@@ -3,33 +3,28 @@ module Chunking
 
   class AdapterMagickImageTest < TestCase
 
+    def max_colour_value
+      Magick::Image::QuantumRange
+    end
+
     def test_initialize
-      image = stub( "magick_image" )
-      adapter = Image::AdapterMagickImage.new( image )
-      assert_equal image, adapter.base_image
+      base_image = stub( "base_image" )
+      adapter = Image::AdapterMagickImage.new( base_image )
+      assert_equal base_image, adapter.base_image
     end
 
-    # TODO: check online if this is too closely testing the implementation
+    # TODO: not sure how to test this adapter without - 
+    # a) testing functionality already testing by RMagick
+    # b) writing heavily mocked test that are brittle.
+
     def test_method_size
-      rows = stub( "rows" )
-      cols = stub( "columns" )
-      image = mock( "magick_image", :rows => rows, :columns => cols )
-      adapter = Image::AdapterMagickImage.new( image )
+      rows = rand( 100 )
+      cols = rand( 100 )
+      base_image = Magick::Image.new( cols, rows )
+      adapter = Image::AdapterMagickImage.new( base_image )
       assert_equal cols, adapter.size( :x )
       assert_equal rows, adapter.size( :y )
     end
 
-    # -----------------
-    # Behavioural tests
-    # -----------------
-
-    def test_method_size_behaviour
-      rows = 1
-      cols = 2
-      magick_image = Magick::Image.new( cols, rows )
-      adapter = Image::AdapterMagickImage.new( magick_image )
-      assert_equal cols, adapter.size( :x )
-      assert_equal rows, adapter.size( :y )
-    end
   end
 end
