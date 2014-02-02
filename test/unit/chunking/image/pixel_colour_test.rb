c5b86b893a099e0a31ec94e8a50d5662ba028a9a
Tim Brown <tim@maplejar.co.uk>
2014-02-02 08:42:59 +0000

    pixel_colour_tests refactored

--- test/unit/chunking/image/pixel_colour_test.rb
+++ test/unit/chunking/image/pixel_colour_test.rb
@@ -16,31 +16,46 @@ module Chunking
       Image::AdapterMagickImage.max_colour_value
     end
 
-    def test_method_compare_single_colours
-      image = Class.extend( Image::PixelColour::ClassMethods )
+    def test_compare_single_colours_method
+      klass = Image::MixedIn
       a, b = (0..max_colour_value).to_a.sample( 2 )
 
-      assert image.compare_single_colours?( a, a )
-      assert !image.compare_single_colours?( a, b )
-      assert image.compare_single_colours?( a, b, (a-b).abs )
-      assert !image.compare_single_colours?( a, b, (a-b).abs-1 )
+      # in/equality
+      assert klass.compare_single_colours?( a, a )
+      assert !klass.compare_single_colours?( a, b )
+
+      # tolerance works
+      required_tolerance = (a-b).abs
+      assert klass.compare_single_colours?( a, b, required_tolerance )
+      assert !klass.compare_single_colours?( a, b, required_tolerance-1 )
     end
 
-    def test_method_compare_colours
-      klass = Class.extend( Image::PixelColour::ClassMethods )
-      r, g, b, a = stub( "r" ), stub( "g" ), stub( "b" ), stub( "a" )
-      c1 = c2 = [ r, g, b, a ]
+    def test_compare_colours_method
+      klass = Image::MixedIn
+      tolerance = stub( "tolerance" )
 
-      klass.stubs( :compare_single_colours? ).returns( true )
-      assert klass.compare_colours?( c1, c2 )
+      r_a, g_a, b_a, a_a = stub( "r_a" ), stub( "g_a" ), stub( "b_a" ), stub( "a_a" )
+      colour_a = [ r_a, g_a, b_a, a_a ]
+      
+      r_b, g_b, b_b, a_b = stub( "r_b" ), stub( "g_b" ), stub( "b_b" ), stub( "a_b" )
+      colour_b = [ r_b, g_b, b_b, a_b ]
 
-      (0..3).each do |i|
-        klass.stubs( :compare_single_colours? ).returns( *Array.new( i, true ) << false )
-        assert !klass.compare_colours?( c1, c2 )
-      end
+      # equality
+      klass.stubs( :compare_single_colours? ).with( r_a, r_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( g_a, g_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( b_a, b_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( a_a, a_b, tolerance ).returns( true )
+      assert klass.compare_colours?( colour_a, colour_b, tolerance )
+
+      # inequality
+      klass.stubs( :compare_single_colours? ).with( r_a, r_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( g_a, g_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( b_a, b_b, tolerance ).returns( true )
+      klass.stubs( :compare_single_colours? ).with( a_a, a_b, tolerance ).returns( false )
+      assert !klass.compare_colours?( colour_a, colour_b, tolerance )
     end
 
-    def test_method_pixel_is_colour
+    def test_pixel_is_colour_method
       image = Image::MixedIn.new
 
       # create dummy values
@@ -51,32 +66,31 @@ module Chunking
       colour_b = stub( "colour_b" )
       colour_x = stub( "colour_x" )
 
-      # assert equality
+      # equality
       image.stubs( :get_pixel_colour ).with( *coords_a ).returns( colour_a )
       image.class.stubs( :compare_colours? ).with( colour_x, colour_a, fuzz ).returns( true )
       assert image.pixel_is_colour?( *coords_a, colour_x, fuzz )
 
-      # assert inequality
+      # inequality
       image.stubs( :get_pixel_colour ).with( *coords_b ).returns( colour_b )
       image.class.stubs( :compare_colours? ).with( colour_x, colour_b, fuzz ).returns( false )
       assert !image.pixel_is_colour?( *coords_b, colour_x, fuzz )
 
-      # it also works for an array of colours.
+      # with an array of colours
       colours_x = [ [ stub( "colours_x1" ) ], [ stub( "colours_x2" ) ] ]
 
-      # assert equality
+      # equality
       image.class.stubs( :compare_colours? ).with( colours_x[0], colour_a, fuzz ).returns( false )
       image.class.stubs( :compare_colours? ).with( colours_x[1], colour_a, fuzz ).returns( false )
       assert !image.pixel_is_colour?( *coords_a, colours_x, fuzz )
 
-      # assert inequality
+      # inequality
       image.class.stubs( :compare_colours? ).with( colours_x[0], colour_b, fuzz ).returns( false )
       image.class.stubs( :compare_colours? ).with( colours_x[1], colour_b, fuzz ).returns( true )
       assert image.pixel_is_colour?( *coords_b, colours_x, fuzz )
     end
 
-    def test_compare_colours_behaviour
-      # This may seem like overkill, but this method is fundamental.
+    def test_compare_colours_method_without_mocks
       image = Class.extend( Image::PixelColour::ClassMethods )
       r, g, b, a, x = (0..max_colour_value).to_a.sample( 5 )
 
@@ -90,21 +104,27 @@ module Chunking
       assert image.compare_colours?( [r, g, b], [r, g, b, a] )
       assert image.compare_colours?( [r, g, b, a], [r, g, b] )
 
-      # inequality at any combination of indexes
+      # tolerance works
+      colour = [r, g, b, a]
+      inequal_colour = colour.reverse
+      required_tolerance = colour.zip( inequal_colour ).map{ |a,b| (a-b).abs }.max
+      assert image.compare_colours?( colour, inequal_colour, required_tolerance )
+      assert !image.compare_colours?( colour, inequal_colour, required_tolerance-1 )
+
+      # inequality at any index / combination of indexes
       # e.g. [[0], [1], [2], [3], [0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3], [0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]]
       possible_index_combinations = (1..3).map{ |i| [0, 1, 2, 3].combination( i ).to_a }.flatten( 1 )
       possible_index_combinations.each do |i|
-        colour = (0..max_colour_value).to_a.sample( 4 )
+        colour = [ r, g, b, a ]
         inequal_colour = (0..3).to_a.map{ |p| i.include?( p ) ? x : colour[p] }
 
-        assert image.compare_colours?( colour, colour.clone )
+        # inequality
         assert !image.compare_colours?( colour, inequal_colour )
 
-        # fuzz (in other words "tolerance")
-        required_fuzz = colour.zip( inequal_colour ).map{ |a,b| (a-b).abs }.max
-        assert image.compare_colours?( colour, inequal_colour, required_fuzz )
-        assert !image.compare_colours?( colour, inequal_colour, required_fuzz-1 )
-
+        # tolerance works
+        required_tolerance = colour.zip( inequal_colour ).map{ |a,b| (a-b).abs }.max
+        assert image.compare_colours?( colour, inequal_colour, required_tolerance )
+        assert !image.compare_colours?( colour, inequal_colour, required_tolerance-1 )
       end
         
     end
