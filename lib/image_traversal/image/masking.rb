6a161581a53240453840aa255694e1b05723d48d
Tim Brown <tim@maplejar.co.uk>
2014-02-17 06:15:06 +0000

    apply_mask opacity now defaults to full

--- lib/image_traversal/image/masking.rb
+++ lib/image_traversal/image/masking.rb
@@ -9,7 +9,7 @@ module ImageTraversal
       end
 
       # Annotate this image using another as a mask
-      def apply_mask( mask, opacity = 5 )
+      def apply_mask( mask, opacity = Palette.max_colour_value )
         new_image = base_image.dissolve( mask.base_image, opacity, 1 )
         self.class.new( new_image )
       end
