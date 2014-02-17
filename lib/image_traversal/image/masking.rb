6e1d4d9a78ac52b0b8319743b1bc99e8a80a9205
Tim Brown <tim@maplejar.co.uk>
2014-02-17 06:22:03 +0000

    apply_mask opacity now defaults to full

--- lib/image_traversal/image/masking.rb
+++ lib/image_traversal/image/masking.rb
@@ -2,14 +2,15 @@ module ImageTraversal
   module Image
     module Masking
 
-      # TODO: review how masking works
+      # TODO: untested?
       # Create a blank image of the same size, but with a transparent background.
       def create_mask( *args )
         self.class.factory( size( :x ), size( :y ) ){ self.background_color = "none" }
       end
 
       # Annotate this image using another as a mask
-      def apply_mask( mask, opacity = Palette.max_colour_value )
+      def apply_mask( mask, opacity = 1 )
+        # TODO: coupled to RMagick
         new_image = base_image.dissolve( mask.base_image, opacity, 1 )
         self.class.new( new_image )
       end
