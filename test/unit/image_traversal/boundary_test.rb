1f5c6fb290080a2faec9a133a385970aa65f9088
Tim Brown <tim@maplejar.co.uk>
2014-02-22 11:37:03 +0000

    housekeeping

--- test/unit/image_traversal/boundary_test.rb
+++ test/unit/image_traversal/boundary_test.rb
@@ -3,8 +3,10 @@ module ImageTraversal
   class BoundaryTest < TestCase
 
     def test_initialize
+      # mock params
       axis, index, absolute_index = stub( "axis" ), stub( "index" ), stub( "absolute_index" )
 
+      # then...
       b = Boundary.new( axis, index, absolute_index )
       assert_equal [ axis, index, absolute_index ], [ b.axis, b.index, b.absolute_index ]
     end
