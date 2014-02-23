98707f360a9058221b48e37da960a54fd8b0be90
Tim Brown <tim@maplejar.co.uk>
2014-02-23 08:36:38 +0000

    housekeeping

--- test/unit/image_traversal/detector_run_test.rb
+++ test/unit/image_traversal/detector_run_test.rb
@@ -3,12 +3,15 @@ module ImageTraversal
   class DetectorRunTest < TestCase
     
     def test_method_add_results
+      # a run with no results.
       run = Detector::Run.new
+      assert_equal [], run.results
+
+      # results to be added.
       result_a = build_result
       result_b = build_result
 
-      assert_equal [], run.results
-
+      # then...
       run.add_result( result_a )
       assert_equal [ result_a ], run.results
 
@@ -16,37 +19,40 @@ module ImageTraversal
       assert_equal [ result_a, result_b ], run.results
     end
 
-    def test_method_add_results_should_increment_tolerance_counter
+    def test_method_add_results_should_increment_tolerance_counter_if_state_changed
+      # a Run with zero tolerance_counter.
       run = Detector::Run.new
-      result_a = build_result( true )
-      result_b = build_result( false )
-
       assert_equal 0, run.send( :tolerance_counter )
 
+      # add a result - should not increment counter.
+      result_a = build_result( true )
       run.add_result( result_a )
       assert_equal 0, run.send( :tolerance_counter )
 
+      # then...
       # if the result does not equal the very first result, increment counter.
+      result_b = build_result( false )
       run.add_result( result_b )
       assert_equal 1, run.send( :tolerance_counter )
     end
 
-    def test_should_annotate_image
-      mask = stub( "mask" )
-      image = mock( "image", :create_mask => mask, :apply_mask => mask )
-
+    def test_should_annotate_results
       run = Detector::Run.new
 
-      # create n run.results, each one expecting :annotate!
-      n = (2..10).to_a.sample
-      n.times do |i|
+      # an image that creates mask.
+      image = build_image
+      mask = build_image
+      image.stubs( :create_mask => mask )
+
+      # then...
+      # create results expecting :annotate!
+      (2..10).to_a.sample.times do |i|
         result = build_result
         result.expects( :annotate! ).with( mask )
         run.add_result( result )
       end
 
-      assert_equal n, run.results.count
-
+      # go
       run.annotate( image )
     end
 
